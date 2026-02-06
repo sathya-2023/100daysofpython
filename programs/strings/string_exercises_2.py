241b95f713e8f23e26647646f4abe9f90c8c3f56
Sathya Narayanan M <sathyanarayanan1107@gmail.com>
2026-02-06 15:08:59 +0530

    prob 2

--- programs/strings/string_exercises_2.py
+++ programs/strings/string_exercises_2.py
@@ -13,6 +13,25 @@ def verbing(s):
             return s + 'ly'
     return s + 'ing'
 
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+
+s = 'This dinner is not that bad!'
+def not_bad(s):
+    # +++your code here+++
+    not_ = s.find('not')
+    bad_ = s.find('bad')
+
+    if not_ != -1 and bad_ != -1 and bad_ > not_:
+        s = s[:not_] + 'good' + s[bad_ + 3:]
+    print(s)
+    return s
 
 
 
